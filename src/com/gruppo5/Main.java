b8f322b32996f46f1da7312fcf53bc8b90ee7d1e
robertaluciano <r.luciano10@studenti.unisa.it>
2021-11-14 16:53:09 +0100

    ...

--- src/com/gruppo5/Main.java
+++ src/com/gruppo5/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("ciao banana");
+        System.out.println("Netbeans è più bello");
     }
 }
